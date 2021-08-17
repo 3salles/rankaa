6819e09acd58844183bdc1c184a4df65fedd2d9c
Beatriz Salles <beatrizsallesss@gmail.com>
2021-08-17 20:39:05 -0300

    refactor: Move card game to its own file

---
+++ src/components/CardGame/index.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { ImLocation } from "react-icons/im";
+
+import {Card, Infos, Tag, Location, Body, TeamContainer, TeamLogo, Date} from './styles'
+
+export function CardGame() {
+  const [showLocation, setShowLocation] = useState(false);
+
+  function handleShowLocation() {
+    setShowLocation((prev) => !prev);
+  }
+  return (
+    <Card>
+      <Infos>
+        <Tag>
+          <img
+            src="https://img.icons8.com/fluency/48/000000/football2.png"
+            alt="Bola de Futsal"
+          />
+          <span>Futsal</span>
+        </Tag>
+        <Location title="Localização" onClick={handleShowLocation}>
+          <ImLocation />
+          {showLocation && <span>UFMA - quadra externa</span>}
+        </Location>
+      </Infos>
+      <Body>
+        <TeamContainer>
+          <TeamLogo
+            src="https://img.icons8.com/color/48/000000/barcelona-fc.png"
+            alt="Brasão da Atlética"
+          />
+          <p>Atletica</p>
+        </TeamContainer>
+        <Date>
+          <p>20/07</p>
+          <span>13:15</span>
+        </Date>
+        <TeamContainer>
+          <TeamLogo
+            src="https://img.icons8.com/color/48/000000/chelsea-fc.png"
+            alt="Brasão da Atlética"
+          />
+          <p>Atletica</p>
+        </TeamContainer>
+      </Body>
+    </Card>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,70 +1,16 @@
-import { useState } from "react";
-import { ImLocation } from "react-icons/im";
-
+import { CardGame } from "../../components/CardGame";
 import { Header } from "../../components/Header";
-import {
-  Container,
-  Title,
-  Section,
-  Card,
-  Infos,
-  Tag,
-  Location,
-  Body,
-  TeamLogo,
-  TeamContainer,
-  Date,
-} from "./styles";
+import { Container, Title, Section } from "./styles";
 
 export function Home() {
-  const [showLocation, setShowLocantion] = useState(false);
-
-  function handleShowLocation() {
-    setShowLocantion((prev) => !prev);
-  }
-
   return (
     <>
       <Header />
       <Container>
         <section>
           <Title>Quartas de Finais</Title>
-          <Section>  
-            <Card>
-              <Infos>
-                <Tag>
-                  <img
-                    src="https://img.icons8.com/fluency/48/000000/football2.png"
-                    alt="Bola de Futsal"
-                  />
-                  <span>Futsal</span>
-                </Tag>
-                <Location title="Localização" onClick={handleShowLocation}>
-                  <ImLocation />
-                  {showLocation && <span>UFMA - quadra externa</span>}
-                </Location>
-              </Infos>
-              <Body>
-                <TeamContainer>
-                  <TeamLogo
-                    src="https://img.icons8.com/color/48/000000/barcelona-fc.png"
-                    alt="Brasão da Atlética"
-                  />
-                  <p>Atletica</p>
-                </TeamContainer>
-                <Date>
-                  <p>20/07</p>
-                  <span>13:15</span>
-                </Date>
-                <TeamContainer>
-                  <TeamLogo
-                    src="https://img.icons8.com/color/48/000000/chelsea-fc.png"
-                    alt="Brasão da Atlética"
-                  />
-                  <p>Atletica</p>
-                </TeamContainer>
-              </Body>
-            </Card>
+          <Section>
+            <CardGame />
           </Section>
         </section>
       </Container>
