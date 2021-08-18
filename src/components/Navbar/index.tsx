36a604f6800c00005d211aa67e14348bda84cba4
Beatriz Salles <beatrizsallesss@gmail.com>
2021-08-18 00:03:33 -0300

    refactor: Create Sidebar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,10 @@
 import { ReducedNav } from "./ReducedNav";
+import { Sidebar } from "./Sidebar";
 
 export function Navbar() {
   return (
     <>
+    <Sidebar />
     <ReducedNav />
     </>
   )
