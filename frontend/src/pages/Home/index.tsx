ce3a1dc877fe4bd22ea415a03fd81c24002ecd00
Beatriz Salles <beatrizsallesss@gmail.com>
2021-08-23 20:29:50 -0300

    refactor: create app layout

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -17,27 +17,30 @@ import {
 import games from "../../utils/games.json";
 import athletics from "../../utils/athletics.json";
 import { Navbar } from "../../components/Navbar";
+import { AppLayout } from "../../layouts/AppLayout";
 
 export function Home() {
   return (
-    <>
-      <Header />
+    <AppLayout>
       <Container>
         <Banner>
-          <img src="https://images.unsplash.com/photo-1561542313-b23c8ff631f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80" alt="Olimpics" />
+          <img
+            src="https://images.unsplash.com/photo-1561542313-b23c8ff631f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80"
+            alt="Olimpics"
+          />
         </Banner>
-        
+
         <div className="desktop">
-        <section className="section">
-          <Title>Quartas de Finais</Title>
-          <BackgroundSection>
-            <Section>
-              {games?.map((game) => (
-                <CardGame key={game.id} game={game} />
-              ))}
-            </Section>
-          </BackgroundSection>
-        </section>
+          <section className="section">
+            <Title>Quartas de Finais</Title>
+            <BackgroundSection>
+              <Section>
+                {games?.map((game) => (
+                  <CardGame key={game.id} game={game} />
+                ))}
+              </Section>
+            </BackgroundSection>
+          </section>
           <section className="section">
             <Title>
               <span>Resultados</span>
@@ -51,19 +54,17 @@ export function Home() {
               ))}
             </div>
           </section>
-         
         </div>
         <section className="section">
-            <Title>
-              <span>Rank</span>
-              <RightButton title="Ver rank completo">
-                <IoIosArrowForward />
-              </RightButton>
-            </Title>
-            <Rank athletics={athletics} />
-          </section>
+          <Title>
+            <span>Rank</span>
+            <RightButton title="Ver rank completo">
+              <IoIosArrowForward />
+            </RightButton>
+          </Title>
+          <Rank athletics={athletics} />
+        </section>
       </Container>
-      <Navbar />
-    </>
+    </AppLayout>
   );
 }
