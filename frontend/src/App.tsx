7bda2dc57c679f39b4995b7ac7327415c1685ce6
Beatriz Salles <beatrizsallesss@gmail.com>
2021-08-23 20:34:59 -0300

    chore: create modality route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/Home';
+import { Modalities } from './pages/Modalities';
 
 import { GlobalStyle } from "./styles/global";
 
@@ -10,7 +11,8 @@ export function App() {
     <GlobalStyle />
     <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={Home}></Route>
+      <Route path="/" exact component={Home} />
+      <Route  path="/modalities" exact component={Modalities} />
     </Switch>
     </BrowserRouter>
     </>
---
+++ frontend/src/pages/Modalities/index.tsx
@@ -0,0 +1,9 @@
+import { AppLayout } from "../../layouts/AppLayout";
+
+export function Modalities (){
+  return (
+    <AppLayout>
+      <h1>Modalities</h1>
+    </AppLayout>
+  )
+}
