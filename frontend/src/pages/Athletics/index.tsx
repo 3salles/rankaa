5339b2f5269a161ec2f4baba6d25e9dbf39d4095
Beatriz Salles <beatrizsallesss@gmail.com>
2021-08-23 21:20:48 -0300

    style: Add style into amount

--- frontend/src/pages/Athletics/index.tsx
+++ frontend/src/pages/Athletics/index.tsx
@@ -1,13 +1,13 @@
 import { AthleticCard } from "../../components/AthleticCard";
 import { AppLayout } from "../../layouts/AppLayout";
 
-import { Container } from './styles'
+import { Container, Amount } from './styles'
 
 export function Athletics () {
   return (
     <AppLayout>
       <Container>
-      <div>Inscritas: 10</div>
+      <Amount>Inscritas: <strong>10</strong> </Amount>
       <div>
         <AthleticCard />
       </div>
--- frontend/src/pages/Athletics/styles.ts
+++ frontend/src/pages/Athletics/styles.ts
@@ -3,3 +3,17 @@ import styled from "styled-components";
 export const Container = styled.section`
   padding: 0 2.25rem 8rem;
 `
+
+export const Amount = styled.p`
+  color: white;
+  text-align: right;
+  font-family: Source Sans Pro;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 1.25rem;
+  margin-bottom: 1rem;
+
+  > span {
+    font-weight: 600;
+  }
+`
