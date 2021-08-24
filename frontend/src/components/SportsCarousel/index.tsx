5f4bc06b2198101ea518d44dbe234640715187d7
Beatriz Salles <beatrizsallesss@gmail.com>
2021-08-23 21:54:42 -0300

    feat: Create button carousel

---
+++ frontend/src/components/SportsCarousel/index.tsx
@@ -0,0 +1,24 @@
+import { Container, Button } from './styles'
+
+export function SportsCarousel () {
+  return (
+    <Container>
+      <Button>
+        <img src="https://img.icons8.com/fluency/48/000000/football2.png" alt="Futsal" />
+        <p>Futsal</p>
+      </Button>
+      <Button>
+        <img src="https://img.icons8.com/fluency/48/000000/football2.png" alt="Futsal" />
+        <p>Futsal</p>
+      </Button>
+      <Button>
+        <img src="https://img.icons8.com/fluency/48/000000/football2.png" alt="Futsal" />
+        <p>Futsal</p>
+      </Button>
+      <Button>
+        <img src="https://img.icons8.com/fluency/48/000000/football2.png" alt="Futsal" />
+        <p>Futsal</p>
+      </Button>
+    </Container>
+  )
+}
---
+++ frontend/src/components/SportsCarousel/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  padding: 0 2.25rem;
+  max-width: 340px;
+  margin-bottom: 1.5rem;
+  display: flex;
+  gap: 1rem;
+  overflow-x: auto;
+
+  &::-webkit-scrollbar {
+    background: 0;
+  }
+`
+
+export const Button = styled.button`
+  background: linear-gradient(137.49deg, #F4A58A 0%, #ED6B4E 96.01%);
+  padding: 16px 24px;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  font-family: Source Sans Pro;
+  font-weight: 600;
+  font-size: 1.125rem;
+  line-height: 1.4375rem;
+  color: white;
+`
