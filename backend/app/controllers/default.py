616fcb4eca818a2435e9d0e7fb4d55bf070908d5
Thallyson Gabriel <87535481+Thalles2310@users.noreply.github.com>
2021-08-31 21:22:18 -0300

    ato2
    atualização da commit anterior

--- backend/app/controllers/default.py
+++ backend/app/controllers/default.py
@@ -1,28 +1,28 @@
 from app import app
-from flask import render_template, request, redirect, jsonify
+from flask import json, render_template, request, redirect, jsonify
 import sqlite3
 
 app.secret_key = 'mykey'
 
+#Home - a pagina inicial da API
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return 'HOME'
 
-@app.route('/entrada')
+#Entrada - Página acessando a Organização (Signup/Login)
 def entrada():
-    return render_template("entrada.html")
+    #Rota de 
+    return 'ENTRADA'
 
-@app.route('/entrada/signup', methods = ['GET', 'POST'])
+#SigUp - Cadastro de um novo organizador
+@app.route('/entrada/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        email = {'email': request.json['email']}
-        name = {'name' : request.json['name']}
-        password = {'password' : request.json['password']}
-        return jsonify(email)
-        """if request.form['name'] != "" or request.form['password'] != "" or request.form['email'] != "":
-            email = request.form['email']
-            name = request.form['name']
-            password = request.form['password']
+        dados = request.json
+        name = dados['name']
+        email = dados['email']
+        password = dados['password']
+        if (name != '') or (email != '') or (password != ''):
             conn = sqlite3.connect("database.db")
             c = conn.cursor()
             c.execute("INSERT INTO users VALUES('"+email+"','"+password+"', '"+name+"')")
@@ -30,47 +30,55 @@ def signup():
             conn.close()
             return jsonify({'mensagem':'Dados enviados'})
         else:
-            return jsonify({'mensagem':'Campos não preenchidos corretamente'})"""
-    return render_template("signup.html")
+            return jsonify({'mensagem':'Dados não enviados'})
+    else:
+        return 'SIGNUP'
 
+#Login - Login de um usuário ja cadastrado
 @app.route('/entrada/login', methods = ['GET', 'POST'])
 def login():
     r = None
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        conn = sqlite3.connect("database.db")
-        c = conn.cursor()
-        c.execute("SELECT * FROM users WHERE email = '"+email+"' and password = '"+password+"'")
-        r = c.fetchall()
-        for i in r:
-            if email == i[0] and password == i[1]:
-                return redirect('http://127.0.0.1:5000/organizacao')
-        return jsonify({'erro':'username ou password incorretos'})
-    return render_template("login.html")
+        dados = request.json
+        email = dados['email']
+        password = dados['password']
+        if (email != "") or (password != ""):
+            conn = sqlite3.connect("database.db")
+            c = conn.cursor()
+            c.execute("SELECT * FROM users WHERE email = '"+email+"' and password = '"+password+"'")
+            r = c.fetchall()
+            for i in r:
+                if email == i[0] and password == i[1]:
+                    return jsonify ({'caminho': 'http://127.0.0.1:5000/organizacao'})
+            return jsonify({'mensagem':'Username ou password incorretos'})
+        else:
+            return jsonify({'mensagem':'Dados não enviados'})
+    else:
+        return 'LOGIN'
 
+#Organização - Pagina inicial da organização
 @app.route('/organizacao')
 def organizacao():
-    return render_template("organizacao.html")
+    return 'ORGANIZAÇÃO'
 
 @app.route('/organizacao/newatletica', methods = ['GET', 'POST'])
 def new_atletica():
-    msg = ''
     if request.method == 'POST':
-        if (request.form['nomeatletica'] == "") or (request.form['curso'] == ""):
-            msg = 'Informações obrigatorias faltando serem preenchidas:\nNome da Atlética\nCurso\nDiretor'
+        dados = request.json
+        nome_atletica = dados['nome_atletica']
+        curso = dados['curso']
+        email = dados['email']
+        facebook = dados['facebook']
+        instagram = dados['instagram']
+        twitter = dados['twitter']
+        if (nome_atletica == "") or (curso == ""):
+            return jsonify({"mensagem":"dados incompletos"})
         else:
-            nome_atletica = request.form['nomeatletica']
-            curso = request.form['curso']
-            email = request.form['email']
-            facebook = request.form['facebook']
-            instagram = request.form['instagram']
-            twitter = request.form['twitter']
             conn = sqlite3.connect("database.db")
             c = conn.cursor()
             c.execute("INSERT INTO atleticas VALUES('"+nome_atletica+"','"+curso+"', '"+email+"', '"+facebook+"', '"+instagram+"', '"+twitter+"')")
             conn.commit()
             conn.close()
-            msg = 'Dados enviados...'
-        return jsonify({'msg':msg})
-    return render_template("nova_atletica.html")
+            return jsonify(dados)
+    else:
+        return 'NOVA ATLETICA'
