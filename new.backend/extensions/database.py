428e8aaef8f5db3b65f74c30953adbb987bd18a7
Mateus Vitor <mateusriograndense@gmail.com>
2021-09-08 00:34:53 -0300

    Extensão de conexão de banco de dados.

---
+++ new.backend/extensions/database.py
@@ -0,0 +1,6 @@
+from tinydb import TinyDB
+
+usuarios_db = TinyDB("database/usuarios.json")
+jogos_db = TinyDB("database/jogos.json")
+atleticas_db = TinyDB("database/atleticas.json")
+modalidades_db = TinyDB("database/modalidades.json")
