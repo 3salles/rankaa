8c039d035c36665a7b55a81913c0c479dd087689
Mateus Vitor <mateusriograndense@gmail.com>
2021-09-09 16:58:01 -0300

    Correção da tabela de usuário e retorno de autenticação de usuário.

--- new.backend/controllers/usuario.py
+++ new.backend/controllers/usuario.py
@@ -1,5 +1,5 @@
 from models.Usuario import Usuario
-from extensions.database import usuarios_db
+from extensions.database import usuarios as table_usuario 
 from tinydb import Query
 from flask import jsonify
 from werkzeug.security import generate_password_hash
@@ -10,24 +10,23 @@ def cadastra_usuario(usuario: dict):
     novo_usuario = usuario
     novo_usuario["password"] = generate_password_hash(novo_usuario["password"])
     usuarios_query = Query()
-    usuario_existe = usuarios_db.search(
+    usuario_existe = table_usuario.contains(
         usuarios_query.email == novo_usuario["email"])
 
-    if(len(usuario_existe)):
+    if(usuario_existe):
         return jsonify({"status": False, "message": "Usuário já cadastrado"}), 400
     else:
         try:
-            usuarios_db.insert(novo_usuario)
+            table_usuario.insert(novo_usuario)
             return jsonify({"status": True, "message": "Success", "data": {"nome": novo_usuario["nome"], "email": novo_usuario["email"]}}), 201
         except Exception as e:
             return jsonify({"status": False, "message": "Erro ao cadastrar usuário", "error": f"{e}"}), 400
 
-
 def autentica_usuario(auth):
     usuarios_query = Query()
-    usuario_existe = usuarios_db.search(
+    usuario_existe = table_usuario.search(
         usuarios_query.email == auth.username[1:-1])
-
+    
     if not auth or not auth.username or not auth.password:
         return jsonify({"status": False, "message": "Could not verify", "WWW-Authenticate": "Basic auth='Login required'"}), 401
 
@@ -36,5 +35,5 @@ def autentica_usuario(auth):
             return jsonify({"status": True, "message": "Success"}), 200
         else:
             return jsonify({"status": False, "message": "Could not verify", "WWW-Authenticate": "Basic auth='Login required'"}), 401
-
-    # return jsonify({"message":"success"})
+    else:
+        return jsonify({"status": False, "message": "Usuário não encontrado"}), 400
